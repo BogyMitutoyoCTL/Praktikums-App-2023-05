68a983e48ecac85281d27bf13dcce6caf44659a4
Thomas Weller <thomas.weller@mitutoyo-ctl.de>
2023-05-31 15:46:07 +0200

    Datenbank Design

--- decision_making_app/lib/main.dart
+++ decision_making_app/lib/main.dart
@@ -1,10 +1,39 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:decision_making_app/StartWidget.dart';
 import 'package:decision_making_app/ZufallsErgebnis.dart';
 import 'package:flutter/material.dart';
 
+class Entscheidung {
+  String fragestellung;
+  List<String> optionen = [];
+
+  Entscheidung(String this.fragestellung) {}
+
+  void add(String option) {
+    optionen.add(option);
+  }
+}
+
+class Datenbank {
+  List<Entscheidung> entscheidungen = [];
+
+  Entscheidung add(String fragestellung) {
+    var neueEntscheidung = Entscheidung(fragestellung);
+    entscheidungen.add(neueEntscheidung);
+    return neueEntscheidung;
+  }
+}
+
+var datenbank = Datenbank();
+
 void main() {
+  var kochen = datenbank.add("Was kochen wir heute?");
+  kochen.add("Nudeln");
+  kochen.add("Spaghetti");
+  kochen.add("Reis");
+  var sport = datenbank.add("Wie halten wir uns heute fit?");
+  sport.add("Fußball");
+  sport.add("Schwimmen");
+
   runApp(const MyApp());
 }
 
@@ -18,22 +47,23 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Decision Maker',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        appBarTheme: AppBarTheme(backgroundColor: Colors.black26, titleTextStyle: TextStyle(fontSize: 20, color: schriftfarbe)),
-        colorScheme: ColorScheme.dark(),
-        textTheme:  TextTheme(displayLarge: TextStyle(fontSize: 28, color: schriftfarbe, fontWeight: FontWeight.bold), displayMedium: TextStyle(fontSize: 20, color: schriftfarbe), displaySmall: TextStyle(fontSize: 15, color: schriftfarbe) )
-      ),
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.blue,
+          appBarTheme: AppBarTheme(backgroundColor: Colors.black26, titleTextStyle: TextStyle(fontSize: 20, color: schriftfarbe)),
+          colorScheme: ColorScheme.dark(),
+          textTheme: TextTheme(
+              displayLarge: TextStyle(fontSize: 28, color: schriftfarbe, fontWeight: FontWeight.bold),
+              displayMedium: TextStyle(fontSize: 20, color: schriftfarbe),
+              displaySmall: TextStyle(fontSize: 15, color: schriftfarbe))),
       home: ZufallsErgebnis(),
     );
   }
 }
-
