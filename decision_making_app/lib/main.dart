ce91b5dd9c3e01829b2a0e02bf9f9b03b4df7864
Thomas Weller <thomas.weller@mitutoyo-ctl.de>
2023-05-31 15:52:41 +0200

    Merge

--- decision_making_app/lib/main.dart
+++ decision_making_app/lib/main.dart
@@ -48,18 +48,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        appBarTheme: AppBarTheme(backgroundColor: Colors.black26, titleTextStyle: TextStyle(fontSize: 20, color: schriftfarbe)),
-        colorScheme: ColorScheme.dark(),
-        textTheme:  TextTheme(displayLarge: TextStyle(fontSize: 28, color: schriftfarbe, fontWeight: FontWeight.bold), displayMedium: TextStyle(fontSize: 20, color: schriftfarbe), displaySmall: TextStyle(fontSize: 15, color: schriftfarbe) )
-      ),
-      home: EditEntscheidungen(),
