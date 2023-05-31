78902f30b1dcd1f1e24ac45e17e4841c8c1f4c96
ileanaa31 <ileana.saibel@protonmail.com>
2023-05-31 14:27:23 +0200

    TextField Edit Optionen

--- decision_making_app/lib/EditOptionen.dart
+++ decision_making_app/lib/EditOptionen.dart
@@ -8,14 +8,47 @@ class EditOptionen extends StatefulWidget {
 }
 
 class _EditOptionenState extends State<EditOptionen> {
+  final List<TextEditingController> _controller = [];
+  List<String> _text = [];
+
+  _EditOptionenState() {
+    for (int i = 0; i < 10; i++) {
+      _text.add("Lorem ipsum 2");
+
+      _controller.add(TextEditingController());
+      _controller[i].text = _text[i];
+      _controller[i].addListener(() {
+        setState(() {
+          _text[i] = _controller[i].text;
+        });
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    final List<TextField> _Textfelder = [];
+    for (TextEditingController _controller in _controller) {
+      _Textfelder.add(TextField(
+        controller: _controller,
+        style: Theme.of(context).textTheme.displaySmall,
+      ));
+    }
     return Scaffold(
       appBar: AppBar(
         title: Center(
           child: Text("Edit Optionen"),
         ),
       ),
+      body: Center(
+          child: Padding(
+              padding: EdgeInsets.all(15),
+              child: Scrollbar(
+                thumbVisibility: true,
+                child: ListView(
+                  children: _Textfelder,
+                ),
+              ))),
     );
   }
 }
--- decision_making_app/lib/main.dart
+++ decision_making_app/lib/main.dart
@@ -6,7 +6,6 @@ import 'package:decision_making_app/StartWidget.dart';
 import 'package:decision_making_app/ZufallsErgebnis.dart';
 import 'package:flutter/material.dart';
 
-
 void main() {
   runApp(const MyApp());
 }
@@ -21,22 +20,28 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Decision Maker',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        appBarTheme: AppBarTheme(backgroundColor: Colors.black26, titleTextStyle: TextStyle(fontSize: 20, color: schriftfarbe)),
-        colorScheme: ColorScheme.dark(),
-        textTheme:  TextTheme(displayLarge: TextStyle(fontSize: 28, color: schriftfarbe, fontWeight: FontWeight.bold), displayMedium: TextStyle(fontSize: 20, color: schriftfarbe), displaySmall: TextStyle(fontSize: 15, color: schriftfarbe) )
-      ),
-      home: EditEntscheidungen(),
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.blue,
+          appBarTheme: AppBarTheme(
+              backgroundColor: Colors.black26,
+              titleTextStyle: TextStyle(fontSize: 20, color: schriftfarbe)),
+          colorScheme: ColorScheme.dark(),
+          textTheme: TextTheme(
+              displayLarge: TextStyle(
+                  fontSize: 28,
+                  color: schriftfarbe,
+                  fontWeight: FontWeight.bold),
+              displayMedium: TextStyle(fontSize: 20, color: schriftfarbe),
+              displaySmall: TextStyle(fontSize: 15, color: schriftfarbe))),
+      home: EditOptionen(),
     );
   }
 }
-
