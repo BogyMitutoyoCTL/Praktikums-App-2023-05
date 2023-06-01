b86c85619c8d6f5b0aa8bac56eb0aeb46ec44362
Mori16tz <moritz.pahlke@web.de>
2023-06-01 14:43:38 +0200

    Resolved Warnings

--- decision_making_app/lib/ZufallsErgebnis.dart
+++ decision_making_app/lib/ZufallsErgebnis.dart
@@ -1,38 +1,35 @@
 // ignore_for_file: prefer_const_constructors
 import 'dart:math';
 import 'package:decision_making_app/Entscheidung.dart';
-import 'package:decision_making_app/Option.dart';
 import 'package:flutter/material.dart';
 
 class ZufallsErgebnis extends StatelessWidget {
   final String frage;
   final Entscheidung entscheidungen;
 
-  const ZufallsErgebnis(String this.frage,
-      Entscheidung this.entscheidungen, {
-        Key? key,
-      }) : super(key: key);
+  const ZufallsErgebnis(
+    this.frage,
+    this.entscheidungen, {
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
           title: Center(
-            child: Text(frage),
-          )),
+        child: Text(frage),
+      )),
       body: Center(
         child: Text(zufallsergebnis(),
-            style: Theme
-                .of(context)
-                .textTheme
-                .displayLarge),
+            style: Theme.of(context).textTheme.displayLarge),
       ),
     );
   }
 
   String zufallsergebnis() {
     var laenge = entscheidungen.optionen.length;
-    Random random = new Random();
+    Random random = Random();
     int randomNumber = random.nextInt(laenge); //from 0 to laenge-1
     String ergebnis = entscheidungen.optionen[randomNumber].toString();
     return ergebnis;
