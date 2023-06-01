ab79facc1a4cfdd532ac674d41d94eee1dbb5202
ileanaa31 <ileana.saibel@protonmail.com>
2023-06-01 12:58:05 +0200

    Frage in Button wird an ZufallsErgebnis weiter gegeben

--- decision_making_app/lib/ZufallsErgebnis.dart
+++ decision_making_app/lib/ZufallsErgebnis.dart
@@ -3,14 +3,17 @@
 import 'package:flutter/material.dart';
 
 class ZufallsErgebnis extends StatelessWidget {
-  const ZufallsErgebnis(String frage, {Key? key}) : super(key: key);
+  const ZufallsErgebnis(
+    String frage, {
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Center(
-            child: Text("Entscheidungshilfe"),
+            child: Text(frage),
           ),
         ),
         body: Center(
