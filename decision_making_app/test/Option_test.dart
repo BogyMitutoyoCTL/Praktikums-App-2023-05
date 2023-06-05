4c53035e603631494313a3912f3be8010e1d9940
ileanaa31 <ileana.saibel@protonmail.com>
2023-06-05 14:17:06 +0200

    Option Encode test

--- decision_making_app/test/Option_test.dart
+++ decision_making_app/test/Option_test.dart
@@ -1,10 +1,11 @@
 // Import the test package and Counter class
+import 'dart:convert';
+import 'package:collection/collection.dart';
 import 'package:decision_making_app/Option.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  group('Option', ()
-  {
+  group('Option', () {
     test('Konstruktor mit Wert...', () {
       final opt = Option("Kaffee");
 
@@ -17,5 +18,13 @@ void main() {
       expect(opt.text, "");
     });
 
+    test('Encode json...', () {
+      final data = Option("testA");
+      var json = jsonDecode(jsonEncode(data));
+      print(json);
+
+      var expected = jsonDecode('{"option": "testA"}');
+      expect(DeepCollectionEquality.unordered().equals(json, expected), true);
+    });
   });
 }
