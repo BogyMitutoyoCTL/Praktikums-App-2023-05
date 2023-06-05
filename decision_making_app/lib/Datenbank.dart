12a6a30ec06432fb1badadb27ddef90bd16346a3
Lukas Benner <lukas.benner@mitutoyo-ctl.de>
2023-06-05 12:06:34 +0200

    fix #22

--- decision_making_app/lib/Datenbank.dart
+++ decision_making_app/lib/Datenbank.dart
@@ -3,6 +3,15 @@ import 'Entscheidung.dart';
 class Datenbank {
   List<Entscheidung> entscheidungen = [];
 
+  Datenbank(){}
+
+  Datenbank.fromJson(Map<String, dynamic> json)
+      : entscheidungen = json['entscheidungen'];
+
+  Map<String, dynamic> toJson() => {
+    'entscheidungen' : entscheidungen
+  };
+
   Entscheidung add(String fragestellung) {
     var neueEntscheidung = Entscheidung(fragestellung);
     entscheidungen.add(neueEntscheidung);
